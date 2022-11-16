0357a6c9170333f23717c161dae193f401e90f8f
Denis Sharov <83408074+denisqaa@users.noreply.github.com>
2022-11-16 14:54:57 +0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const getPlayerName = () => {
-  console.log('Welcome to the brain games!');
+  console.log('Welcome to the Brain games!');
   const name = readlineSync.question('May I have your name?');
   console.log(`Hello, ${name}`);
   return name;
